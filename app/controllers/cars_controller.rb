9aaa4b34a687bc38cf4fbfe1543c1bf484ce4a9b
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-09 15:55:55 -0500

    adds cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,15 @@
 class CarsController < ApplicationController
+
+  def index
+    cars = Car.all
+    render json: {message: cars}
+  end
+
+  def show
+    car = Car.find_by(id: params[:id])
+    render json: {message: car}
+  end
+
+
+
 end
