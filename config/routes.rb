0ac10afce8be79c7b0491dded4badbae9ff62252
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-09 15:37:50 -0500

    adds controller and seeds db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  get '/cars' => 'cars#index' 
+  get '/cars/:id' => 'cars#show'
+  post '/cars' => 'cars#create'
+  patch '/cars/:id' => 'cars#update'
+  delete '/cars/:id' => 'cars#destroy'
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+
+Car.create(make: "Nissan", model: "Altima", color: "White")
+
+Car.create(make: "Toyota", model: "Camry", color: "Black")
+
+Car.create(make: "Honda", model: "Pilot", color: "Silver")
