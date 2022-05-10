3db2fccc84dfe7b465a6e6889f5750849aa4206d
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-09 19:41:39 -0500

    adds views

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -10,6 +10,23 @@ class CarsController < ApplicationController
     render json: {message: car}
   end
 
+  def create
+    car = Car.create(
+      make: params[:make],
+      model: params[:model], 
+      color: params[:color]
+    )
 
+    render json: {message: car}
+  end
+
+  def update
+    car = Car.find_by(id: params[:id])
+    car.make = params[:make]
+    car.model = params[:model]
+    car.color = params[:color]
+    car.save
+    render json: {message: car}
+  end
 
 end
